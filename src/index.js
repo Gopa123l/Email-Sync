a57a8adc22a71af191a7b85e06d6b6f2f36674ac
Gopa123l <f20200278@pilani.bits-pilani.ac.in>
2024-02-02 21:20:29 +0530

    Email Sync service code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const socketIO= require('socket.io');
 const cors= require('cors');
-const http= require('http');
+const app= express();
+require('dotenv').config();
+
+const PORT = 5004;
+const MONGODB_URI= process.env.MONGODB_URI;
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
+
